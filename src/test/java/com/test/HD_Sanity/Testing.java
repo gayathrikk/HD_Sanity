aaca9fd0d0cfca85c8eaefbf00aa16c477424d5f
Gayathrigayu <36566787+gayathrikk@users.noreply.github.com>
2024-03-18 15:26:07 +0530

    Testing.java

--- src/test/java/com/test/HD_Sanity/Testing.java
+++ src/test/java/com/test/HD_Sanity/Testing.java
@@ -40,7 +40,7 @@ public class Testing {
 		{
 			
 			  DesiredCapabilities dc = DesiredCapabilities.chrome();
-		        URL url = new URL("http://172.20.23.7:5555/wd/hub");
+		        URL url = new URL("http://172.12.20.99:4444/wd/hub");
 		        driver = new RemoteWebDriver(url, dc);
 		
 		       
@@ -85,7 +85,7 @@ public class Testing {
 	        }
 	        WebElement emailInput = wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath("//input[@type='email']")));
 	        if (emailInput != null && emailInput.isDisplayed()) {
-	            emailInput.sendKeys("teamsoftware457@gmail.com");
+	            emailInput.sendKeys("softwareteam45@gmail.com");
 	            System.out.println("Email was entered successfully.");
 	        } else {
 	           System.out.println("Email was not entered.");
